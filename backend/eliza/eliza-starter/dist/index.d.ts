ec623f0abf9d02d382531679cd84cfe27bb5457e
Arnav-panjla <arnavpanjla@gmail.com>
2025-06-29 02:07:30 +0530

    eliza - validator

--- backend/eliza/eliza-starter/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Character, AgentRuntime } from '@elizaos/core';
-
-declare const wait: (minTime?: number, maxTime?: number) => Promise<unknown>;
-declare function createAgent(character: Character, db: any, cache: any, token: string): AgentRuntime;
-
-export { createAgent, wait };
